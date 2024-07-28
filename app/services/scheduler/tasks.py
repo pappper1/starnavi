ad5d08ab1aec14e68a43c3664874fb92f5c42f01
pappper1 <rewkruterbyby@gmail.com>
2024-07-28 16:55:34 +0200

    added scheduler and task for ai answer

---
+++ app/services/scheduler/tasks.py
@@ -0,0 +1,17 @@
+from app.post.comment.repository import CommentDAO
+from app.services.ai.chat_gpt import chat_gpt
+
+
+class SchedulerTasks:
+
+	@staticmethod
+	async def ai_answer_comment(
+			post_id: int, post_title: str, post_content: str, comment_content: str
+	) -> None:
+		ai_answer = await chat_gpt.answer_post_comment(
+			post_title=post_title,
+			post_content=post_content,
+			comment_content=comment_content
+		)
+
+		await CommentDAO.add(content=ai_answer, post_id=post_id)
